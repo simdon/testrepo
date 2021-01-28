964392618d0fa356ad0bb932b8dee731b49c7758
simdon <simon@donohue.uk>
2021-01-28 19:36:52 +0000

    added date

--- tester.sh
+++ tester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "test" ]; then
-        echo "TEST COMPLETED ON HOST: $(hostname)"
+        echo "TEST COMPLETED ON HOST: $(hostname) at $(date)"
     else
-        echo "RAN ON HOST: $(hostname)"
+        echo "RAN ON HOST: $(hostname) at $(date)"
 fi
